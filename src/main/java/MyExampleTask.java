233f68e1eaf215dd7a0662bd60c46ff2638274d4
kostiantyn.doroshenko <ku4er007@gmail.com>
2020-09-29 15:47:16 +0300

    simple

--- src/main/java/MyExampleTask.java
+++ src/main/java/MyExampleTask.java
@@ -2,7 +2,7 @@ public class MyExampleTask {
     public static void main(String[] args) {
         String e = "JavA Hillel";
         System.out.println(e);
-        int i = 8;
+        int i = 9;
 
         System.out.println(e + i++);
         System.out.println("Simple text"+ " " + e + " " + i);
