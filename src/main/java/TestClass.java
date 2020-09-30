71441e047891bdd791c51b876dfd0978012f8651
kostiantyn.doroshenko <ku4er007@gmail.com>
2020-09-30 18:05:54 +0300

    uuuu commit 2

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -4,7 +4,7 @@ public class TestClass {
     public static void main(String[] args) {
 
 
-        String t = "Vasy11548941";
+        String t = "Vasy11548adf941";
         System.out.println(t);
         System.out.println(t + 1);
     }
