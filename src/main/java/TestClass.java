b0f1fe8caffbe593871b1ed735104f334c4bfdeb
kostiantyn.doroshenko <ku4er007@gmail.com>
2020-09-30 17:42:27 +0300

    Vasy

---
+++ src/main/java/TestClass.java
@@ -0,0 +1,10 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class TestClass {
+    public static void main(String[] args) {
+
+
+        String t = "Vasy";
+        System.out.println(t);
+    }
+}
