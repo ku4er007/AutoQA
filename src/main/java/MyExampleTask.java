2b338e28181ce4aa98fc62db28de884ecdab2b97
kostiantyn.doroshenko <ku4er007@gmail.com>
2020-09-30 10:06:30 +0300

    test

--- src/main/java/MyExampleTask.java
+++ src/main/java/MyExampleTask.java
@@ -5,7 +5,7 @@ public class MyExampleTask {
         int i = 9;
 
         System.out.println(e + i++);
-        System.out.println("Simple text"+ " " + e + " " + i);
+        System.out.println("Simple text0"+ " " + e + " " + i);
         System.out.println("Say Hi");
         System.out.println("No money no honey");
         System.out.println("I love GTA 5 text text text ...");
